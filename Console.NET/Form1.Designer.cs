592c8c586cdec92f7c8ea4798f85d03ddb6dce2a
Alexander Forselius <drsounds@gmail.com>
2012-08-19 21:07:40 +0200

    First commit
    First commit

---
+++ Console.NET/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Console.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 374);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(846, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(846, 374);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(24, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(681, 82);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(711, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(712, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Topmost";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 474);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Console .NET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+
+    }
+}
+
---
+++ Console.NET/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime;
+using System.Diagnostics;
+namespace Console.NET
+{
+    public partial class Form1 : Form
+    {
+        public delegate void DataReceived(String data);
+        public Form1()
+        {
+            InitializeComponent();
+            p = new Process();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String app = textBox1.Text.Split(' ')[0];
+            String command = textBox1.Text.Split(' ').Length > 1 ? textBox1.Text.Split(new char[]{' '}, 2)[1] : "";
+
+           string cmdexePath = @"C:\Windows\System32\cmd.exe";
+            //notice the quotes around the below string...
+            string myApplication = command;
+            //the /K keeps the CMD window open - even if your windows app closes
+            //string cmdArguments = String.Format("/K {0} {1}", app, command);
+            ProcessStartInfo psi = new ProcessStartInfo(cmdexePath);
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardError = true;
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+           
+            
+            p.StartInfo = psi;
+            p.Start();
+            try
+            {
+                p.BeginOutputReadLine();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        Process p;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p.StandardInput.WriteLine(textBox1.Text);
+         
+           
+        }
+        void DataReceivedMethod(String data)
+        {
+            richTextBox1.HideSelection = false;
+
+            richTextBox1.AppendText( data + "\n");
+            
+        }
+        void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            richTextBox1.Invoke(new Form1.DataReceived(DataReceivedMethod),  e.Data);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = checkBox1.Checked;
+        }
+    }
+}
